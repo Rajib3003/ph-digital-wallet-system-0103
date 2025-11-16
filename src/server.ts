8770da83356fa42d4bcff9d46d355a98da1a9db3
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-11-16 12:27:01 +0600

    database connect done on port 5000

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import {Server} from "http";
+import mongoose from "mongoose";
+import app from "./app";
+
+let server: Server;
+
+
+const serverStart = async () => {
+    const port = 5000;
+
+    try {
+
+        await mongoose.connect("mongodb+srv://mongodb:mongodb@cluster0.qgah9aq.mongodb.net/digital-wallet-db?retryWrites=true&w=majority&appName=Cluster0");
+        console.log("Database connected");
+        server = app.listen(port, ()=>{
+            console.log(`server started on port ${port}`)
+        })
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+serverStart();
