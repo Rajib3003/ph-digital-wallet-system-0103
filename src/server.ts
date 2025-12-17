11ba183a805ea6e8757d9a4f51be7d9a772799f3
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-12-17 14:28:56 +0600

    google cloud consle new add

--- src/server.ts
+++ src/server.ts
@@ -7,22 +7,16 @@ import { seedSuperAdmin } from "./app/utils/seedSuperAdmin";
 let server: Server;
 
 
-const serverStart = async () => {
-    // const port = 5000;
-
+const serverStart = async () => {  
     try {
-
-        await mongoose.connect(envVar.DB_URL);
+        await mongoose.connect(envVar.DB_URL as string);
         console.log("Database connected");
         server = app.listen(envVar.PORT, ()=>{
             console.log(`server started on port ${envVar.PORT}`)
-        })
-        
+        })        
     } catch (error) {
         console.log(error);
     }
-
-
 }
 
 (async()=> {
@@ -35,6 +29,40 @@ const serverStart = async () => {
 
 process.on("unhandledRejection", (error) => {
     console.log("Unhandled Rejection is detected, we are closing our server....",error);
+    if (server) {
+        server.close(() => {
+            console.log("Server closed");
+            process.exit(1);
+        })}
+
+        process.exit(1);
+
+})
+
+process.on("uncaughtException", (error) => {
+    console.log("uncaught Exception is detected, we are closing our server....",error);
+    if (server) {
+        server.close(() => {
+            console.log("Server closed");
+            process.exit(1);
+        })}
+
+        process.exit(1);
+
+})
+process.on("SIGTERM", () => {
+    console.log("SIGTERM singnal recieved, we are closing our server....");
+    if (server) {
+        server.close(() => {
+            console.log("Server closed");
+            process.exit(1);
+        })}
+
+        process.exit(1);
+
+})
+process.on("SIGINT", () => {
+    console.log("SIGINT singnal recieved, we are closing our server....");
     if (server) {
         server.close(() => {
             console.log("Server closed");
